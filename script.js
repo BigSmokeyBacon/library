f0ba16603fbe68976b3b0512b83c343ecf62ea20
Tadgh Purcell <tadghp@gmail.com>
2023-02-28 16:15:26 +0000

    Store books in library array so won't delete when new one entered

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 "Use Strict";
 const bookDisplay = document.querySelector(".book-display");
+const overlay = document.querySelector(".overlay");
 const newBookForm = document.querySelector("form");
 const btnSubmit = document.querySelector(".btn-submit");
 const btnNew = document.querySelector(".btn-new");
@@ -8,7 +9,9 @@ const authorInput = document.querySelector("#author");
 const pagesInput = document.querySelector("#pages");
 const statusInput = document.querySelector("#status");
 
-const myLibrary = [];
+const myLibrary = [
+  { title: "Harry Potter", author: "J.K. Rowling", pages: 345 },
+];
 
 const Book = function (title, author, pages, read) {
   this.title = title;
@@ -46,15 +49,52 @@ btnSubmit.addEventListener("click", function (e) {
   //Display New Book on Page
   bookDisplay.innerHTML = "";
   myLibrary.forEach((book, i) => {
-    book.divEl = document.createElement("div");
-    book.divEl.textContent = book.info();
-    book.divEl.setAttribute("index", i);
-    bookDisplay.appendChild(book.divEl);
+    bookDisplay.insertAdjacentHTML(
+      "afterbegin",
+
+      `<div class="card card--${i}">
+      <div class="card-title">${book.title}</div>
+      <div class="card-author">${book.author}</div>
+      <div class="card-pages">${book.pages}</div>
+      <button class="btn btn-card-read" type="button">Read</button>
+      <button class="btn btn-card-remove" type="button">Remove</button>
+      <!-- <div class="card-title">J.K. Rowling</div>
+      <div class="card-title">J.K. Rowling</div> -->
+    </div>`
+    );
   });
   //Clear input fields
   clearInputs();
+  closeModal();
 });
 
-btnNew.addEventListener("click", function () {
-  newBookForm.style.opacity = 100;
+const closeModal = function () {
+  newBookForm.classList.toggle("hidden");
+  overlay.classList.toggle("hidden");
+};
+
+btnNew.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !overlay.classList.contains("hidden")) {
+    closeModal();
+  }
 });
+
+overlay.addEventListener("click", closeModal);
+
+const createCard = function () {
+  const card = document.createElement("div");
+  const cardTitle = document.createElement("div");
+  const cardAuthor = document.createElement("div");
+  const cardPages = document.createElement("div");
+  const cardBtnRead = document.createElement("button");
+  const cardBtnRemove = document.createElement("button");
+
+  document.appendChild(card);
+  document.appendChild(cardTitle);
+  document.appendChild(cardAuthor);
+  document.appendChild(cardPages);
+  document.appendChild(cardBtnRead);
+  document.appendChild(cardBtnRemove);
+};
